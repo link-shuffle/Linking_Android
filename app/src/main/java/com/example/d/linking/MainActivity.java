6bbb339e0f982dde6e694e5f950d48bbd03e9e98
jeongyeon <yeon8480@gmail.com>
2019-11-02 23:37:08 +0900

    Google Login finished

--- app/src/main/java/com/example/d/linking/MainActivity.java
+++ app/src/main/java/com/example/d/linking/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.d.linking;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton googlelogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //email 가져오기
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        //구글 로그인 클라이언트 객체 초기화
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googlelogin = (SignInButton)findViewById(R.id.googlelogin);
+
+        googlelogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("정보", "connected to Google Play");
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("정보", "Connection Failed");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN) {
+            //로그인 intent 꺼진 후에 로그인 결과 처리.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void  handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            Log.d("정보", "Sign In Result: Failed Code = " + e.getStatusCode());
+            updateUI(null);
+        }
+
+    }
+
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if(account != null) {
+            Intent intent1 = new Intent(getApplicationContext(), WorkSpace.class);
+            startActivity(intent1);
+        }else {
+            Toast.makeText(getApplicationContext(), "email does not exist.", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/d/linking/WorkSpace.java
@@ -0,0 +1,7 @@
+package com.example.d.linking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkSpace extends AppCompatActivity {
+
+}
